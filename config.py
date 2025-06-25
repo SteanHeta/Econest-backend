d0084d3d3b47b19e1b7b29caee8f9287f07c00bf
Stean <stean.ngugi@student.moringaschool.com>
2025-06-25 22:33:04 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,22 @@
+# backend/config.py
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.getenv('JWT_SECRET_KEY') 
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+ 
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+
+
+    SWAGGER = {
+        'title': 'Eco-Nest Market API',
+        'uiversion': 3,
+        "specs_route": "/apidocs/"
+    }
+    GOOGLE_CLIENT_ID = os.getenv('GOOGLE_CLIENT_ID')
+    GOOGLE_CLIENT_SECRET = os.getenv('GOOGLE_CLIENT_SECRET')
