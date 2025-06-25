59d23bf6e5423c7777d2d35fcc1a79e2d53db445
Stean <stean.ngugi@student.moringaschool.com>
2025-06-25 22:35:15 +0300

    Initial commit

---
+++ extensions.py
@@ -0,0 +1,16 @@
+# backend/extensions.py
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+from flasgger import Swagger
+from authlib.integrations.flask_client import OAuth
+from flask_cors import CORS
+
+db = SQLAlchemy()
+migrate = Migrate()
+bcrypt = Bcrypt()
+jwt = JWTManager()
+swagger = Swagger()
+oauth = OAuth()
+cors = CORS()
