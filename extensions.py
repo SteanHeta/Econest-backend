777f33e1cecfaad67be2b85e53293590ad2a4885
Stean <stean.ngugi@student.moringaschool.com>
2025-06-30 01:37:26 +0300

    Final fixes for DATABASE_URL env var and conflicting Firebase init

--- extensions.py
+++ extensions.py
@@ -7,7 +7,6 @@ from flask_jwt_extended import JWTManager
 from flasgger import Swagger
 from authlib.integrations.flask_client import OAuth
 from flask_cors import CORS
-from firebase_admin import credentials
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -16,22 +15,6 @@ jwt = JWTManager()
 swagger = Swagger()
 cors = CORS()
 oauth = OAuth()
-
-firebase = None
-
-FIREBASE_SERVICE_ACCOUNT_KEY_PATH = os.getenv('FIREBASE_SERVICE_ACCOUNT_KEY_PATH')
-
-if FIREBASE_SERVICE_ACCOUNT_KEY_PATH:
-    try:
-        if os.path.exists(FIREBASE_SERVICE_ACCOUNT_KEY_PATH):
-            cred = credentials.Certificate(FIREBASE_SERVICE_ACCOUNT_KEY_PATH)
-            firebase = firebase_admin.initialize_app(cred)
-        else:
-            print(f"[Firebase] Error: Key file not found at {FIREBASE_SERVICE_ACCOUNT_KEY_PATH}")
-    except Exception as e:
-        print(f"[Firebase] Error initializing Firebase Admin SDK: {e}")
-else:
-    print("[Firebase] Warning: FIREBASE_SERVICE_ACCOUNT_KEY_PATH not set. Firebase not initialized.")
 
 def init_oauth(app):
     oauth.init_app(app)
